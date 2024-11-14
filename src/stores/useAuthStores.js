88d1744671908e289e1ada5c01de2be3c5c9d787
mysk423 <80307373+mysk423@users.noreply.github.com>
2024-11-14 10:46:40 +0900

    가벼운 수정 및 pinia stores 가이드 추가

--- src/stores/useAuthStores.js
+++ src/stores/useAuthStores.js
@@ -1,12 +1,32 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
 export const useAuthStore = defineStore('auth', () => {
     const token = ref('');
+    const userDetail = reactive({
+        userId : '',
+        userDescription : '',
+        userImageUrl : '',
+    })
 
+    /*
+        로그인 성공시 저장할 데이터들.
+    */
+
+    // 엑세스 토큰을 저장시켜주세요.
+    // 해당 토큰은 axios 전송시 인터셉터로 선처리 할 예정입니다.
     function setToken(accessToken){
         token.value = accessToken
     }
 
-    return { token , setToken}
+    // 가벼운 로그인 유저의 정보를 담아둡니다. 캐싱해서 매번 서버에
+    // 유저 정보를 끌어올 필요 없이 pinia에서 가벼운 정보들만 가져와서 
+    // 쓸 수 있으면 좋을 것 같습니다.
+    function setUserDetail(data){
+        userDetail.userId = data.userId;
+        userDetail.userDescription = data.description;
+        userDetail.userImageUrl = data.userImageUrl;
+    }
+
+    return { token , setToken, setUserDetail }
 })
