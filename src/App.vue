f0e53d28cab0b40210a042b9c6aaae0f405d8ef8
mysk423 <80307373+mysk423@users.noreply.github.com>
2024-11-18 20:48:38 +0900

    css 수정중

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,9 @@
 
 <template>
   <div class="row">
-
     <div class="col-3">
       <SideBar/>
     </div>
-
     <div class="col-9">
       <RouterView/>
     </div>
@@ -17,8 +15,7 @@
     
 </template>
 
-<style lang="css">
-  
+<style lang="css">  
   a {
     text-decoration: none;
   }
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -3,15 +3,6 @@
     <div class="post">
         <MainPosts/>
     </div>
-    <div class="post">
-        <MainPosts/>
-    </div>
-    <div class="post">
-        <MainPosts/>
-    </div>
-    <div class="post">
-        <MainPosts/>
-    </div>
     <div class="end-posts">
         <EndPosts/>
     </div>
@@ -28,34 +19,29 @@ import EndPosts from '@/components/child/EndPosts.vue';
 <style>
 
 .container {
-  overflow-y: auto;
-  overflow-x: hidden;
-  height: 100%; /* 부모 높이에 맞춤 */
+    overflow-y: auto;
+    overflow-x: hidden;
+    flex-direction: column;
+    height: 100%;
 }
 
 .post {
-  background-color: #1e1e1e;
-  border: 1px solid #2c2c2c;
-  border-radius: 3px;
-  margin-bottom: 20px;
-  width: 100%;
+    background-color: #1e1e1e;
+    border: 1px solid #2c2c2c;
+    border-radius: 3px;
+    margin-bottom: 20px;
+    width: 100%;
 }
 .end-posts {
-  background-color: #1e1e1e;
-  border: 1px solid #2c2c2c;
-  border-radius: 3px;
-  margin-bottom: 60px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    background-color: #1e1e1e;
+    border: 1px solid #2c2c2c;
+    border-radius: 3px;
+    margin-bottom: 60px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-@media (max-width: 1200px) {
-.container {
-max-width: calc(100% - 40px);
-padding: 20px;
-}
-}
 
 </style>
