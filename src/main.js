db8c9e6bf2e3593148dde23444c2be42e4e9e9fa
yoojihun <dbwlgns1204@gmail.com>
2024-11-19 17:10:53 +0900

    pinia 추가

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import axios from './util/axios';
 import App from './App.vue';
 import SideBar from './components/SideBar.vue';
 import router from './router/router';
+import { createPinia } from 'pinia';
 
-
+const pinia = createPinia();
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 app.component('SideBar', SideBar);
 app.use(router);
+app.use(pinia);
 app.mount('#app');
